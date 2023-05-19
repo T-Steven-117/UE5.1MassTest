5a78c9e91a03677f81e6cafbc8adcc8e7088b5a8
Steven <Steven-117>
2023-05-19 15:54:46 +0800

    update

---
+++ Source/G510/MassAI/MassMoveTask.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MassMoveTask.h"
+
+#include "MassStateTreeExecutionContext.h"
+
+bool FMassMoveTask::Link(FStateTreeLinker& Linker)
+{
+	return true;
+}
+
+EStateTreeRunStatus FMassMoveTask::Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const
+{
+	return EStateTreeRunStatus::Succeeded;
+}
+
+
+EStateTreeRunStatus FMassMoveTask::EnterState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const
+{
+	return EStateTreeRunStatus::Failed;
+}
